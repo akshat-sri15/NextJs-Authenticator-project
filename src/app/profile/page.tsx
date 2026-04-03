3a2746ff4db8874e7e93580014d2cdd3d80a7d07
Akshat Srivastava <akshatsrivastavavk@gmail.com>
2026-04-04 00:30:21 +0530

    modify profile page

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -2,6 +2,7 @@ export default function ProfilePage(){
     return(
         <div className="flex justify-center items-center min-h-screen bg-gradient-to-br from-gray-900 via-gray-800 to-gray-900">
             <h1 className="text-4xl font-bold text-white">
+                
                 Welcome to your common profile!
             </h1>
         </div>
